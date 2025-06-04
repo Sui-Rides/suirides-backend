0ba697f3e03d5e49cbe338f48b66790f6cc603eb
oumoumag <ouma.godwin10@gmail.com>
2025-06-04 23:30:58 +0300

    feat: implement upload function for Walrus SDK

--- walrus/walrus.js
+++ walrus/walrus.js
@@ -3,3 +3,9 @@ const walrus = require('walrus-sdk');
 walrus.config({
     apiKey: process.env.WALRUS_API_KEY,
 });
+
+const uploadToWalrus = async (file) => {
+    const blobId = await walrus.upload(file);
+    return blobId;
+};
+
